29c1dff6b154965a27b45ee557274e02c909408f
FernandoColchones <130452951+FernandoColchones@users.noreply.github.com>
2023-04-24 09:27:33 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,15 @@
-input.onGesture(Gesture.Shake, function () {
+radio.onReceivedNumber(function (receivedNumber) {
     tool = randint(0, 2)
     if (tool == 0) {
         basic.showIcon(IconNames.SmallSquare)
-    } else if (tool == 1) {
+    } else if (tool == 0) {
         basic.showIcon(IconNames.Square)
     } else {
         basic.showIcon(IconNames.Scissors)
     }
+})
+input.onGesture(Gesture.Shake, function () {
+    radio.sendNumber(777)
 })
 let tool = 0
 radio.setGroup(777)
